f22bbe2a0282c93fab1962cbba5fbf435c09660b
Tim Oltjenbruns <tim.oltjenbruns@softvision.com>
2022-05-25 17:18:11 -0400

    WIP baseline merger

---
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/report/BaselineMergeTask.kt
@@ -0,0 +1,36 @@
+package io.gitlab.arturbosch.detekt.report
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+@CacheableTask
+abstract class BaselineMergeTask : DefaultTask() {
+
+    @get:InputFiles
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val input: ConfigurableFileCollection
+
+    @get:OutputFile
+    abstract val output: RegularFileProperty
+
+    @TaskAction
+    fun merge() {
+        logger.info("Input")
+        logger.info(input.files.joinToString(separator = "\n") { it.absolutePath })
+        logger.info("Output = ${output.get().asFile.absolutePath}")
+        val existingFiles = input.files.filter { it.exists() }
+        fun isXmlReport(file: File): Boolean = file.name.endsWith(".xml")
+        if (existingFiles.any(::isXmlReport)) {
+            BaselineMerger.merge(existingFiles.filter(::isXmlReport), output.get().asFile)
+            logger.lifecycle("Merged XML output to ${output.get().asFile.absolutePath}")
+        }
+    }
+}
---
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/report/BaselineMerger.kt
@@ -0,0 +1,56 @@
+package io.gitlab.arturbosch.detekt.report
+
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+
+object BaselineMerger {
+
+    private val documentBuilder by lazy { DocumentBuilderFactory.newInstance().newDocumentBuilder() }
+
+    fun merge(inputs: Collection<File>, output: File) {
+        val document = documentBuilder.newDocument().apply {
+            xmlStandalone = true
+            val baselineNode = createElement("SmellBaseline")
+            baselineNode.appendChild(createElement("ManuallySuppressedIssues"))
+            baselineNode.appendChild(createElement("CurrentIssues"))
+            appendChild(baselineNode)
+        }
+
+        inputs
+            .filter { it.exists() }
+            .flatMap { currentIssues(it) }
+            .distinctBy { it.textContent }
+            .forEach {
+                document.documentElement.appendChild(document.importNode(it, true))
+            }
+
+        TransformerFactory.newInstance().newTransformer().run {
+            setOutputProperty(OutputKeys.INDENT, "yes")
+            setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2")
+            transform(DOMSource(document), StreamResult(output.writer()))
+        }
+    }
+
+    private fun currentIssues(it: File) =
+        documentBuilder
+            .parse(it.inputStream())
+            .documentElement
+            .childNodes.asSequence().first {
+                it.nodeName == "CurrentIssues"
+            }.childNodes.asSequence().filterNot {
+                it.nodeType == Node.TEXT_NODE && it.textContent.isBlank()
+            }
+
+}
+
+private fun NodeList.asSequence() = sequence {
+    for (index in 0 until length) {
+        yield(item(index))
+    }
+}
